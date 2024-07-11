856b031e19c5212faa7e193452ad3eed1129d4e5
Chrystine Martins <chrystine.martins@outlook.com>
2024-07-11 19:44:18 -0300

    update index.html and index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+const criarItemDeAtividade = () => {
+    return `
+    
+     <div>
+
+            <input type="checkbox">
+
+            <span>Academia em grupo</span>
+
+            <time>Sábado, 18 de abril às 08:00h</time>
+
+        </div>
+    
+    
+    `
+}
+
+
 
 const section = document.querySelector('section')
 
